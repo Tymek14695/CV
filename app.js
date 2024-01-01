20ed32476b80d05efa45ad6c4aa9db9c13d4a2ce
Tymek14695 <14695@eans-nt.edu.pl>
2024-01-01 17:11:16 +0100

    Created a local server, modified index.html, created app.js with small functionalities as showing contact info

---
+++ app.js
@@ -0,0 +1,6 @@
+function getEmailAdress() {
+    document.getElementById("showEmail").innerHTML = "timszarlej@gmail.com"
+}
+function getPhoneNumber() {
+    document.getElementById("showNumber").innerHTML = "(+48) 518 997 219"
+}
