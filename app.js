53b1751ae62f98663dcf7ad5547d79e57c030072
Tymek14695 <148887712+Tymek14695@users.noreply.github.com>
2024-01-09 17:00:50 +0100

    added js and upgraded index.html

---
+++ app.js
@@ -0,0 +1,6 @@
+function getEmailAdress() {
+    document.getElementById("showEmail").innerHTML = "<p>timszarlej@gmail.com</p>"
+}
+function getPhoneNumber() {
+    document.getElementById("showNumber").innerHTML = "<p>(+48) 518 997 219</p>"
+}
